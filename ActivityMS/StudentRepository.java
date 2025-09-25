65a822f912103b526aea5908864855389a578c5d
Samuel Rosero <p3g455o48@gmail.com>
2025-09-25 16:06:13 -0500

    feat: Added in memory student repository and implemented repository logic

--- ActivityMS/StudentRepository.java
+++ ActivityMS/StudentRepository.java
@@ -1,5 +1,11 @@
 package ActivityMS;
 
+import java.util.List;
+
 public interface StudentRepository {
-    
+    void saveStudent(Student student);
+    Student getStudentById(String id);
+    void deleteStudentById(String id); //Optional way of deleting
+    void updateStudent(Student student); //Optional method
+    List <Student> getAllStudents();
 }
